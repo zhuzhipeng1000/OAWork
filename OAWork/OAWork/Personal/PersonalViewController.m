0fe7bcfad13022fd3f52a340035e1a88c05e6bcf
james <zhuzhipeng@foreveross.com>
2017-12-11 17:16:19 +0800

    **feature**

--- OAWork/OAWork/Personal/PersonalViewController.m
+++ OAWork/OAWork/Personal/PersonalViewController.m
@@ -10,8 +10,27 @@
 #import "OaMainCellTableViewCell.h"
 #import "ForgetPassWordViewController.h"
 #import "PersonDetailViewController.h"
+#import "User.h"
+#import <MJRefresh/MJRefresh.h>
+#import <SDWebImage/UIImageView+WebCache.h>
+#import <MBProgressHUD/MBProgressHUD.h>
+#import "NHPopoverViewController.h"
+#import "TailoringViewController.h"
+#import "WQAlert.h"
+#import <AVFoundation/AVFoundation.h>
+#import <AFNetworking/AFNetworking.h>
 
-@interface PersonalViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@interface PersonalViewController ()<UITableViewDelegate,UITableViewDataSource,UINavigationControllerDelegate,UIImagePickerControllerDelegate>{
+    UIButton *_bar;
+    BOOL _isEdit;
+    UIScrollView *backScroll;
+    UIImageView *headImage;
+    UITextField *nameTf;
+    NHPopoverViewController *ReBacInfoView;
+    UIImage *selectedimv;
+    BOOL _isReviseHeadView;
+}
 @property (nonatomic,strong) NSMutableArray *allArray;
 
 @end
@@ -20,23 +39,311 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    _isReviseHeadView=false;
+    
      self.title=@"个人设置";
+    _isEdit=false;
     self.navigationController.title=@"个人设置";
     [self createNaviTopBarWithShowBackBtn:false showTitle:YES];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(imgeFromLoacl) name:@"HEADiMAGEcHANGED" object:nil];
+    
+    backScroll=[[UIScrollView alloc]initWithFrame:CGRectMake(0, TOPBARCONTENTHEIGHT+1, SCREEN_WIDTH, SCREEN_HEIGHT-BOTTOMBARHEIGHT)];
+    backScroll.backgroundColor=[Utils colorWithHexString:@"#f7f7f7"];
+    [self.view addSubview:backScroll];
+    
+    UIView *headBack=[[UIView alloc]initWithFrame:CGRectMake(0, 0, SCREEN_WIDTH, 155)];
+    headBack.backgroundColor=[UIColor whiteColor];
+    [backScroll addSubview:headBack];
+    
+    headImage=[[UIImageView alloc]initWithFrame:CGRectMake((SCREEN_WIDTH-86)/2, 20, 86, 86)];
+    [self imgeFromLoacl];
+    __weak __typeof(self) weakSelf = self;
+    backScroll.mj_header =[MJRefreshNormalHeader headerWithRefreshingBlock:^{
+        //Call this Block When enter the refresh status automatically
+       
+    }];
+    [backScroll addSubview:headImage];
+    
+    nameTf=[[UITextField alloc]initWithFrame:CGRectMake(0, headImage.bottom, SCREEN_WIDTH, 40)];
+    nameTf.font=[UIFont systemFontOfSize:17.0f];
+    nameTf.textAlignment=NSTextAlignmentLeft;
+    
+    nameTf.text=[User shareUser].userName;
+    [backScroll addSubview:nameTf];
+    
+    UIButton *headBt=[[UIButton alloc]initWithFrame:CGRectMake(headImage.left, headImage.top, headImage.width, headImage.height)];
+    headBt.backgroundColor=[UIColor clearColor];
+    [headBt addTarget:self action:@selector(headBtTapped:) forControlEvents:UIControlEventTouchUpInside];
+    [backScroll addSubview:headBt];
+    
+
+    
+    UIImageView *arrImage=[[UIImageView alloc]initWithFrame:CGRectMake(nameTf.left, nameTf.bottom+10, 6, 11)];
+    arrImage.image=[UIImage imageNamed:@"left_arrow"];
+    [backScroll addSubview:arrImage];
 
     _allArray = [@[@"手机号码",@"邮箱",@"部门",@"个人图章"] mutableCopy];
     
-    _demoTableView=[[UITableView alloc]initWithFrame:CGRectMake(0, TOPBARCONTENTHEIGHT+1, SCREEN_WIDTH-20, SCREEN_HEIGHT-50)];
-    [_demoTableView registerNib:[UINib nibWithNibName:@"OaMainCellTableViewCell" bundle:nil] forCellReuseIdentifier:@"OaMainCellTableViewCell"];
-    _demoTableView.delegate=self;
-    _demoTableView.dataSource=self;
-    _demoTableView.separatorStyle=UITableViewCellSeparatorStyleNone;
-    [self.view addSubview:_demoTableView];
+    for (int d=0; d<_allArray.count; d++) {
+        NSDictionary *dic=_allArray[d];
+        UIView *smallBig=[[UIView alloc]initWithFrame:CGRectMake(0, 44*d+headBack.bottom, SCREEN_WIDTH, 44)];
+        smallBig.backgroundColor=[UIColor whiteColor];
+        [backScroll addSubview:smallBig];
+        
+        UIImageView *imav=[[UIImageView alloc]initWithFrame:CGRectMake(20, 10, 20, 20)];
+        imav.image=[UIImage imageNamed:@""];
+        [smallBig addSubview:imav];
+        
+        UILabel *lb=[[UILabel alloc]initWithFrame:CGRectMake(imav.right, 0, 70, 44)];
+        lb.font=[UIFont systemFontOfSize:14.0];
+        lb.textColor=[UIColor blackColor];
+        [smallBig addSubview:lb];
+        
+        UITextField *nameTf=[[UITextField alloc]initWithFrame:CGRectMake(lb.right, lb.top, SCREEN_WIDTH-lb.right-20, lb.height)];
+        nameTf.font=[UIFont systemFontOfSize:17.0f];
+        nameTf.textAlignment=NSTextAlignmentLeft;
+       [smallBig addSubview:nameTf];
+        
+        UIView *bottomStrait=[[UIView alloc]initWithFrame:CGRectMake(0,smallBig.bottom-1, smallBig.width, 1)];
+        bottomStrait.backgroundColor=[Utils colorWithHexString:@"#e4e4e4"];
+        if (d!=(_allArray.count-1)) {
+            [smallBig addSubview:bottomStrait];
+        }
+    
+    }
+    
+    UIButton *revisePass=[[UIButton alloc]initWithFrame:CGRectMake(0, 44*(_allArray.count)+headBack.bottom+30, SCREEN_WIDTH, 44)];
+    revisePass.backgroundColor=[UIColor whiteColor];
+    [revisePass setTitle:@"保存" forState:UIControlStateNormal];
+    [revisePass setTitle:@"保存" forState:UIControlStateHighlighted];
+    [revisePass setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+     [revisePass setTitleColor:[UIColor blackColor] forState:UIControlStateHighlighted];
+    [revisePass addTarget:self action:@selector(revisePass:) forControlEvents:UIControlEventTouchUpInside];
+    [backScroll addSubview:revisePass];
+    
+//    _demoTableView=[[UITableView alloc]initWithFrame:CGRectMake(0, headBack.bottom, SCREEN_WIDTH, SCREEN_HEIGHT-50)];
+//    [_demoTableView registerNib:[UINib nibWithNibName:@"OaMainCellTableViewCell" bundle:nil] forCellReuseIdentifier:@"OaMainCellTableViewCell"];
+//    _demoTableView.delegate=self;
+//    _demoTableView.dataSource=self;
+//    _demoTableView.separatorStyle=UITableViewCellSeparatorStyleNone;
+//    [self.view addSubview:_demoTableView];
     // Do any additional setup after loading the view from its nib.
 }
 - (void)viewWillAppear:(BOOL)animated{
     [super viewWillAppear:animated];
     self.navigationController.navigationBarHidden=true;
+    if (!_bar) {
+        _bar=[[UIButton alloc]init];
+        [_bar setTitle:@"..." forState: UIControlStateNormal];
+        [_bar setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [_bar setTitle:@"..." forState: UIControlStateHighlighted];
+        [_bar setTitleColor:[UIColor blackColor] forState:UIControlStateHighlighted];
+        [_bar addTarget:self action:@selector(editTapped:) forControlEvents:UIControlEventTouchUpInside];
+        _bar.frame=CGRectMake(SCREEN_WIDTH-60, 0, 60, 44);
+        [self.view addSubview:_bar];
+    }
+    _bar.hidden=false;
+}
+
+-(void)editTapped:(UIButton*)bt{
+    _isEdit=!_isEdit;
+    if (_isEdit) {
+        [bt setTitle:@"保存" forState:UIControlStateNormal];
+        [bt setTitle:@"保存" forState:UIControlStateHighlighted];
+        nameTf.userInteractionEnabled=YES;
+        nameTf.layer.cornerRadius=5.0f;
+        nameTf.layer.borderWidth=1.0f;
+        nameTf.layer.borderColor=[UIColor greenColor].CGColor;
+        nameTf.clipsToBounds=true;
+    }else{
+        if (_isReviseHeadView) {
+            [self uploadAttach];
+        }
+        nameTf.userInteractionEnabled=false;
+        nameTf.layer.cornerRadius=0.0f;
+        nameTf.layer.borderWidth=0.0f;
+        [bt setTitle:@"..." forState:UIControlStateNormal];
+        [bt setTitle:@"..." forState:UIControlStateHighlighted];
+    }
+    
+}
+-(void)imgeFromLoacl{
+    
+    [headImage sd_setImageWithURL:[NSURL URLWithString:[Utils getNotNullNotNill:[User shareUser].icon]] placeholderImage:[UIImage imageNamed:@"main_head_deafult"]];
+    headImage.layer.cornerRadius=headImage.width/2;
+    headImage.clipsToBounds=YES;
+}
+-(void)headBtTapped:(UIButton*)bt{
+    [self.view endEditing:YES];
+    if (!ReBacInfoView) {
+        UIView *headTapPopView=[[UIView alloc]initWithFrame:CGRectMake(20, 0,SCREEN_WIDTH-40, 120)];
+        NSArray *titles=@[@"从手机相册中选择",@"拍一张图片",@"取消"];
+        for (int d=0; d<titles.count; d++) {
+            UIButton *button2 = [UIButton buttonWithType:UIButtonTypeCustom];
+            [button2 setTitle:titles[d] forState:UIControlStateNormal];
+            [button2 setTitle:titles[d] forState:UIControlStateHighlighted];
+            button2.tag=1000+d;
+            [button2 setTitleColor:[Utils colorWithHexString:@"#363636"] forState:UIControlStateNormal];
+            [button2 addTarget:self action:@selector(chooseImage:)
+              forControlEvents:UIControlEventTouchUpInside];
+            button2.frame=CGRectMake(0,40*d, headTapPopView.width, 40);
+            [headTapPopView addSubview:button2];
+            UIView *bottomStrait=[[UIView alloc]initWithFrame:CGRectMake(0,headTapPopView.bottom-1, headTapPopView.width, 1)];
+            bottomStrait.backgroundColor=[Utils colorWithHexString:@"#e4e4e4"];
+            if (d!=(titles.count-1)) {
+                [headTapPopView addSubview:bottomStrait];
+            }
+            
+        }
+         ReBacInfoView = [[NHPopoverViewController alloc] initWithView:headTapPopView contentSize:headTapPopView.frame.size autoClose:FALSE];
+        [ReBacInfoView show];
+    }
+
+}
+-(void)chooseImage:(UIButton*)bt{
+    
+    if (bt.tag==1001) {
+        [self takePic];
+    }else if (bt.tag==1001) {
+        [self takePic];
+    }else{
+        [ReBacInfoView dismiss];
+    }
+}
+-(void)revisePass:(UIButton*)bt{
+    
+    UIViewController *avc=[[ForgetPassWordViewController alloc]initWithNibName:@"ForgetPassWordViewController" bundle:nil];
+ 
+    [self.navigationController pushViewController:avc animated:YES];
+}
+-(void)takePic{
+    {
+        UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypeCamera;
+        if ([UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera]) {
+            if ([self checkCameraAuthorization]) {
+                UIImagePickerController* picker = [[UIImagePickerController alloc] init];
+                picker.delegate = self;
+                picker.sourceType = sourceType;
+                if([[[UIDevice currentDevice] systemVersion] floatValue]>=8.0) {
+                    self.modalPresentationStyle=UIModalPresentationOverCurrentContext;
+                }
+                [self presentViewController:picker animated:YES completion:^{
+                }];
+            }
+        }
+    }
+}
+-(void)selecteFromLib{
+    UIImagePickerControllerSourceType sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
+    UIImagePickerController* picker = [[UIImagePickerController alloc] init];
+    picker.delegate = self;
+    picker.sourceType = sourceType;
+    if([[[UIDevice currentDevice] systemVersion] floatValue]>=8.0) {
+        self.modalPresentationStyle=UIModalPresentationOverCurrentContext;
+    }
+    [self presentViewController:picker animated:YES completion:^{
+    }];
+    
+    
+}
+- (void)imagePickerController:(UIImagePickerController *)picker didFinishPickingMediaWithInfo:(NSDictionary<NSString *,id> *)info {
+    TailoringViewController* tailoringVC = [[TailoringViewController alloc] init];
+    tailoringVC.sourceImage  = info[UIImagePickerControllerOriginalImage];
+    tailoringVC.picker = picker;
+    tailoringVC.myBlockImage = ^ (UIImage* image){
+        headImage.image = image;
+        selectedimv=image;
+        NSString *path=NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask,YES)[0];
+        path = [path stringByAppendingPathComponent:@"fileName"];
+        
+        NSData *data = UIImagePNGRepresentation(image);
+        BOOL isscuess= [data writeToFile:path atomically:YES];
+        
+        if (isscuess) {
+            NSLog(@"bao chun cheng gong");
+            _isReviseHeadView=YES;
+        }
+        
+        [[NSNotificationCenter defaultCenter] postNotificationName:@"HEADiMAGEcHANGED" object:nil];
+    };
+    [picker pushViewController:tailoringVC animated:YES];
+}
+
+- (BOOL)checkCameraAuthorization
+{
+    BOOL isAvalible = YES;
+    //ios 7.0以上的系统新增加摄像头权限检测
+    if ([Utils isIOS7]) {
+        //获取对摄像头的访问权限。
+        AVAuthorizationStatus authStatus = [AVCaptureDevice authorizationStatusForMediaType:AVMediaTypeVideo];
+        switch (authStatus) {
+            case AVAuthorizationStatusRestricted:
+                break;
+            case AVAuthorizationStatusDenied:
+                isAvalible = NO;
+                break;
+            case AVAuthorizationStatusAuthorized:
+                break;
+            case AVAuthorizationStatusNotDetermined:
+                isAvalible =[UIImagePickerController isSourceTypeAvailable:UIImagePickerControllerSourceTypeCamera];
+                break;
+            default:
+                break;
+        }
+    }
+    if (!isAvalible) {
+        NSDictionary* infoDict =[[NSBundle mainBundle] infoDictionary];
+        NSString *appName =[infoDict objectForKey:@"CFBundleDisplayName"];
+        [WQAlert showAlertViewControllerWithTitle:@"" message:[NSString stringWithFormat:@"您关闭了%@的相机权限，无法进行拍照。可以在手机 > 设置 > 隐私 > 相机中开启权限。", appName] buttonTitles:@[@"确定"] showViewController:self completionBlock:^(int index) {
+            [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString] ];
+            
+        } cancleBlock:^{
+            
+        } preferredStyle:UIAlertControllerStyleAlert];
+        
+    }
+    return isAvalible;
+}
+-(void)uploadAttach{
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    //接收类型不一致请替换一致text/html或别的
+    
+    manager.responseSerializer.acceptableContentTypes = [NSSet setWithObjects:@"application/json",
+                                                         @"text/html",
+                                                         @"image/jpeg",
+                                                         @"image/png",
+                                                         @"application/octet-stream",
+                                                         @"text/json",
+                                                         nil];
+    NSMutableDictionary *dic=[NSMutableDictionary dictionary];
+    
+    [dic  setObject:@"image" forKey:@"fileType"];
+    NSURLSessionDataTask *task = [manager POST:@"http://14.23.156.164:8810/mobile/file/upload.jhtml" parameters:dic constructingBodyWithBlock:^(id<AFMultipartFormData> _Nonnull formData) {
+        //            NSData *data = UIImagePNGRepresentation(selectedimv);
+        float kCompressionQuality = 0.3;
+        NSData *data = UIImageJPEGRepresentation(selectedimv, kCompressionQuality);
+        [formData appendPartWithFileData:data name:@"uploadFiles" fileName:[NSString stringWithFormat:@"%@.jpg",[Utils randomUUID]] mimeType:@"application/octet-stream"];
+        
+        
+    } progress:^(NSProgress *_Nonnull uploadProgress) {
+        NSLog(@"上传进度%@",uploadProgress);
+        //打印下上传进度
+    } success:^(NSURLSessionDataTask *_Nonnull task, id _Nullable responseObject) {
+        //上传成功
+        if ([responseObject isKindOfClass:[NSDictionary class]]&&[responseObject[@"errrorCode"] intValue]==200) {
+            if ([responseObject[@"data"] isKindOfClass:[NSArray class]]&&[responseObject[@"data"] count] >0) {
+                
+            }
+            
+        }
+        
+        NSLog(@"上传成功%@",responseObject);
+    } failure:^(NSURLSessionDataTask *_Nullable task, NSError * _Nonnull error) {
+        //上传失败
+        NSLog(@"上传失败");
+    }];
+    [task  resume];
 }
 #pragma mark UITableViewDelegate
 - (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
